ca24a72093eea2589de6f4eaa62d12e93b454a67
Devs-JavaScript <kauad9092@gmail.com>
2024-09-04 16:07:45 -0300

    Add files via upload

---
+++ roultes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const route = express.Router()
+const cors = require('cors')
+const cursoControler = require('/controllers/CursoController')
+
+route.options("*", cors())
+
+//route text
+
+route.get('/curso', cursoController.findALLTurmas)
+route.post('/curso', cursoController.saveCurso)
+route.put('/curso/:id', cursoControler.updateCurso)
+route.delete('/curso/:id', cursoControler.deleteCurso)
+
+module.exports = route
